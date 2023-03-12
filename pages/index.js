e9ce166deb21e145f969a0c018aa0ea8f2e59713
Sebastien Zasada <sezasada@icloud.com>
2023-03-12 11:35:19 -0500

    added another site to my portfolio and adjusted the zoom when hovering

--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,13 @@ import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
 import deved from "../public/dev-ed-wave.png";
 import mypicture from "../public/mypicture.png"
+import feedback from '../public/Feedback.png';
 import code from "../public/code.png";
 import design from "../public/design.png";
 import consulting from "../public/consulting.png";
 import Image from "next/image";
 import MarketWatcher from "../public/MarketWatcher.png"
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
+
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -147,12 +143,14 @@ export default function Home() {
             <div className="basis-1/3 flex-1 ">
               <h3 style={{ display: "flex", justifyContent: "center", textAlign: "center", marginBottom: "10px" }} className="text-3xl py-1 dark:text-white">Market Watcher</h3>
               <div style={{ overflow: "hidden", position: "relative" }}>
-                <Image
-                  style={{ width: "100%", height: "100%", objectFit: "cover", transition: "transform 0.5s ease", position: "relative", zIndex: 1 }}
-                  src={MarketWatcher}
-                  onMouseOver={(e) => { e.target.style.transform = "scale(1.4) translateZ(0)" }}
-                  onMouseOut={(e) => { e.target.style.transform = "scale(1) translateZ(0)" }}
-                />
+                <a href="https://marketwatcher.fly.dev/#/home" target="_blank" rel="noopener noreferrer">
+                  <Image
+                    style={{ width: "100%", height: "100%", objectFit: "cover", transition: "transform 0.5s ease", position: "relative", zIndex: 1 }}
+                    src={MarketWatcher}
+                    onMouseOver={(e) => { e.target.style.transform = "scale(1.4) translateZ(0)" }}
+                    onMouseOut={(e) => { e.target.style.transform = "scale(1) translateZ(0)" }}
+                  />
+                </a>
               </div>
             </div>
 
@@ -177,12 +175,15 @@ export default function Home() {
             </div>
             <div className="basis-1/3 flex-1">
               <h3 style={{ display: "flex", justifyContent: "center", textAlign: "center", marginBottom: "10px" }} className="text-3xl py-1 dark:text-white">Feedback Loop</h3>
-              <Image
-                style={{ width: "100%", height: "100%", objectFit: "cover", transition: "transform 0.5s ease", position: "relative", zIndex: 1 }}
-                src={MarketWatcher}
-                onMouseOver={(e) => { e.target.style.transform = "scale(1.4)" }}
-                onMouseOut={(e) => { e.target.style.transform = "scale(1)" }}
-              />
+              <a href="https://feedbackloop.fly.dev/#/" target="_blank" rel="noopener noreferrer">
+                <Image
+                  style={{ width: "100%", height: "100%", objectFit: "cover", transition: "transform 0.5s ease", position: "relative", zIndex: 1 }}
+                  src={feedback}
+                  onMouseOver={(e) => { e.target.style.transform = "scale(1.6)"; e.target.style.transformOrigin = "top" }}
+                  onMouseOut={(e) => { e.target.style.transform = "scale(1)"; e.target.style.transformOrigin = "center" }}
+                />
+
+              </a>
             </div>
           </div>
         </section>
