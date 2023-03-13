63843bac3617ebdffcac630f9a9dd9202053f8b5
Sebastien Zasada <sezasada@icloud.com>
2023-03-12 21:40:58 -0500

    added my resume to project

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import {
   AiFillLinkedin,
   AiFillYoutube,
 } from "react-icons/ai";
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
 import deved from "../public/dev-ed-wave.png";
@@ -16,10 +19,11 @@ import Image from "next/image";
 import MarketWatcher from "../public/MarketWatcher.png"
 import Movies from "../public/Movies.png";
 import EarningsData from "../public/EarningsData.png";
+import Resume from "./resume";
 
-
-export default function Home() {
+function Home() {
   const [darkMode, setDarkMode] = useState(false);
+
 
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -40,9 +44,13 @@ export default function Home() {
                 />
               </li>
               <li>
-                <a
+                {/* <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
                   href="#"
+                >
+                  Resume
+                </a> */}
+                <a href="/resume" className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
                 >
                   Resume
                 </a>
@@ -61,77 +69,16 @@ export default function Home() {
               establishing a foundation that will allow me to immediately contribute as an entry level software engineer.
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-              <AiFillTwitterCircle />
-              <AiFillLinkedin />
-              <AiFillYoutube />
+              <a href="https://www.linkedin.com/in/sebastien-zasada-b0545a259/" target="_blank">
+
+                <AiFillLinkedin />
+              </a>
             </div>
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
               <Image src={mypicture} />
             </div>
           </div>
         </section>
-        {/* <section>
-          <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
-          </div>
-          <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-              <Image src={design} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs
-              </h3>
-              <p className="py-2">
-                Creating elegant designs suited for your needs following core
-                design theory.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={code} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">
-                Code your dream project
-              </h3>
-              <p className="py-2">
-                Do you have an idea for your next great website? Let's make it a
-                reality.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={consulting} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-              <p className="py-2">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div>
-          </div>
-        </section> */}
         <section className="py-10">
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Portfolio</h3>
@@ -195,3 +142,4 @@ export default function Home() {
     </div>
   );
 }
+export default Home;
---
+++ pages/resume.js
@@ -0,0 +1,150 @@
+import React from 'react';
+
+function Resume() {
+    return (
+        <div>
+            <div>
+                <div className="split left">
+                    <div className="centered right">
+                        <div>
+                            <h1>SEBASTIEN ZASADA</h1>
+                        </div>
+                        <div>
+                            <ol>
+                                <p>&#9993 sezasada@icloud.com</p>
+                                <p>&#128222 (218) 790-0487</p>
+                                <p>&#10003 4115 58th St S, Fargo, ND 58104</p>
+                            </ol>
+                        </div>
+                        <div>
+                            <h1>EDUCATION</h1>
+                        </div>
+                        <br />
+                        <div>
+                            <h3>NORTH DAKOTA STATE UNIVERSITY</h3>
+                            <p>SOPHMORE (2021-2022)</p>
+                            <p>Fargo, ND</p>
+                            <p>Undergraduate Finance</p>
+                        </div>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>Crypto Club</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>UNIVERSITY OF ST. THOMAS</h3>
+                        <p> FRESHMAN (2020-2021)</p>
+                        <p> St. Paul, MN</p>
+                        <p>Undergraduate Finance</p>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>Club Hockey Team</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>OAK GROVE LUTHERAN</h3>
+                        <p>Fargo, ND</p>
+                        <p>Graduated with high honors, 2020</p>
+                    </div>
+                    <div>
+                        <awards>
+                            <div>
+                                <p>Awards & Honors</p>
+                                <div>
+                                    <div>
+                                        <ul>
+                                            <li>Honor roll student</li>
+                                            <li>Philanthropy and Youth leader</li>
+                                        </ul>
+                                    </div>
+                                    <p>Extracurricular Activities</p>
+                                    <ul>
+                                        <li>Fargo South hockey</li>
+                                        <li>Team Captain, 2019-2020</li>
+                                        <li>All EDC Defensive Player 2020</li>
+                                        <li>All-State Defensive Player, 2020</li>
+                                        <li>Oak Grove golf team</li>
+                                        <li>Oak Grove trap shooting team</li>
+                                    </ul>
+                                </div>
+
+                                <div>
+                                    <div className="split right">
+                                        <div className="centered left">
+
+                                            <div>
+                                                <h1>ADDITIONAL SKILLS</h1>
+                                            </div>
+                                            <div>
+                                                <p>Basic knowledge of the stock market and</p>
+                                                <p>crypto industry</p>
+                                                <p>Effective, engaged communicator</p>
+                                                <p>Relationship builder</p>
+                                                <p>Basic understanding of Solidity and Java</p>
+                                                <p>Programming languages</p>
+                                            </div>
+                                            <div>
+                                                <h1>WORK EXPERIENCE</h1>
+                                            </div>
+                                            <br />
+                                            <div>
+                                                <p>GOLF CLUB BAG ROOM EMPLOYEE</p>
+                                            </div>
+                                            <div>
+                                                <p>Fargo Country Club, Fargo, ND / May 2017-Aug 2019</p>
+                                                <ul>
+                                                    <li>Positively engaged with and respond to club member requests</li>
+                                                    <li>Cleaned club members' golf clubs</li>
+                                                    <li>Organized/cleaned bag room, pro shop and bathrooms</li>
+                                                    <li>Set up, took down and maintained the driving range</li>
+                                                </ul>
+                                            </div>
+                                            <div>
+                                                <h3>FOCUS GROUP COORDINATOR</h3>
+                                                <p>ByteSpeed, Moorhead, MN / Aug 2018-Jan 2019</p>
+                                                <ul>
+                                                    <li>Managed a focus group of 20 professors and teachers,</li>
+                                                    <li>guiding them through the basics of virtual reality programs</li>
+                                                    <li>Tested new VR applications daily</li>
+                                                    <li>Developed VR reports and gave VR companies feedback</li>
+                                                </ul>
+                                                <p>on their applications</p>
+                                                <ul>
+                                                    <li>Oversaw feedback reports from focus group participants</li>
+                                                    <li>and sent them to various VR companies</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <h3>ENTREPRENUER</h3>
+                                            <p>Window Washing Business/May 2018-present</p>
+
+                                            <ul>
+                                                <li>Window washing services</li>
+                                                <li>Manage inventory, scheduling and finances</li>
+                                                <li>Focused marketing to retirees and high value homes</li>
+                                            </ul>
+                                        </div>
+                                        <div>
+                                            <bold><b>REFERENCES</b></bold>
+                                            <p>MARK JOHNSON Head Golf Pro, Fargo Country Club</p>
+                                            <p>(701) 261-6810 - mark.johnson@fargocc.com</p>
+                                            <p>STEVE BURIAN Business Mentor and Personal Reference</p>
+                                            <p>(701) 740-4881 - steve.burianassociates.com</p>
+                                            <p>ANNA HANSON Sales Director, ByteSpeed</p>
+                                            <p>(701) 200-1472 - ahanson@bytespeed.com</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </awards>
+                    </div>
+                </div>
+            </div>
+        </div >
+    );
+}
+
+export default Resume;
