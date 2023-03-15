36c8b26ecd55b39dbaa89dd8df5c2ddbb300a966
Sebastien Zasada <sezasada@icloud.com>
2023-03-14 21:20:38 -0500

    changed the background color , added new resume information

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
