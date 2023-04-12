4aa66def521ee2d409daa4ad749627ac1addf673
Sebastien Zasada <sezasada@icloud.com>
2023-04-12 10:17:51 -0500

    single quotes do not work in next.js

--- pages/index.js
+++ pages/index.js
@@ -76,10 +76,10 @@ function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Projects</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              As a junior developer, I've been expanding my skills in
-              Express.js, Javascript, React.js, PostgreSQL, HTML, CSS, and I'm
+              As a junior developer, I&apos;ve been expanding my skills in
+              Express.js, Javascript, React.js, PostgreSQL, HTML, CSS, and I&apos;m
               always on the lookout for new challenges and projects to tackle. I
-              believe that coding is both an art and a science, and I'm
+              believe that coding is both an art and a science, and I&apos;m
               committed to crafting elegant, efficient solutions that meet the
               needs of my clients and users.
             </p>
