dfa10d511e8f386c8032cec255d01b2b40064de3
sezasada <sezasada@icloud.com>
2024-06-07 10:22:56 -0500

    removed utils folder. Added formattedMessage to SubHeading component

--- pages/Components/SubHeading.js
+++ pages/Components/SubHeading.js
@@ -1,3 +1,5 @@
+import { FormattedMessage } from "react-intl";
+
 const SubHeading = () => {
   return (
     <div className="divs-div">
@@ -6,16 +8,13 @@ const SubHeading = () => {
         style={{ maxWidth: "1000px", margin: "0 auto" }}
       >
         <div className="center-title">
-          <div className="text-3xl py-1 dark:text-white">Projects</div>
+          <div className="text-3xl py-1 dark:text-white">
+            <FormattedMessage id="SubHeading.title" />
+          </div>
         </div>
 
         <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-          As a junior developer, I&apos;ve been expanding my skills in
-          Javascript, React.js, PostgreSQL, MySQL, C#, HTML, CSS, and I&apos;m
-          always on the lookout for new challenges and projects to tackle. I
-          believe that coding is both an art and a science, and I&apos;m
-          committed to crafting elegant, efficient solutions that meet the needs
-          of my clients and users.
+          <FormattedMessage id="SubHeading.bio" />
         </p>
       </div>
     </div>
